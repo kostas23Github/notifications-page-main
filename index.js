219bf5b81b3c1b0b8c17b1a9a4392d026ae32a8b
kostas <akwstas1@gmail.com>
2022-09-29 16:25:06 +0300

    Add mobile view responsivness. Finals code, styling modifications

--- index.js
+++ index.js
@@ -1,46 +1,40 @@
+/* DOM ELEMENTS */
+
 const notificationsContainer = document.querySelector(".notifications-container");
+let unreadIndex = document.querySelector('.unreadIndex');
+let allRead = document.querySelector(".set-read");
 
-// Reads notifications
-let notifications = Array.from(notificationsContainer.children);
-
-notifications.forEach(notification => notification.addEventListener('click', () => {
-    notification.setAttribute("class", "notification read");
-    notification.removeEventListener('click', () => {
-        notification.setAttribute("class", "notification read");
-    })
-}));
+/* FUNCTIONS */
 
 // Handles header's notification counter
-let unreadIndex = document.querySelector('.unreadIndex');
-const unreadNotifications = () => {
-    let header = document.querySelector(".header");
+const unreadNotificationsCounter = () => {
+    let notificationsBanner = document.querySelector(".notificationsBanner");
+    let notificationsBannerHidden = document.querySelector(".notificationsBannerHidden");
     let noOfUnread = document.querySelectorAll(".unread").length;
     
     if (noOfUnread === 1) {
         unreadIndex.textContent = noOfUnread;
     } else if (noOfUnread === 0) {
-        return header.innerHTML = `All read`;
+        notificationsBanner.style.display = "none";
+        notificationsBannerHidden.style.display = "block";
     } else {
-        return unreadIndex.textContent = noOfUnread;
+        unreadIndex.textContent = noOfUnread;
     }
 }
 
-window.onload = unreadNotifications();
-notifications.forEach(notification => notification.addEventListener('click', unreadNotifications));
-
 // Handles header's Mark all as read
-
 const markAllAsRead = () => {
     let unreadNotifications = document.getElementsByClassName("notification");
+    let notificationsBanner = document.querySelector(".notificationsBanner");
+    let notificationsBannerHidden = document.querySelector(".notificationsBannerHidden");
     for (let i = 0; i < unreadNotifications.length; i++) {
         unreadNotifications[i].setAttribute("class", "notification read");
     }
-    return document.querySelector(".header").innerHTML = `All read`;
+    notificationsBanner.style.display = "none";
+    notificationsBannerHidden.style.display = "block";
 }
 
-let allRead = document.querySelector(".set-read");
-allRead.addEventListener('click', markAllAsRead);
-
+// Toggles the response message element of the notification
 const toggleMsg = () => {
     let msg = document.querySelector('.msg');
     
@@ -51,8 +45,24 @@ const toggleMsg = () => {
     }
 }
 
-document.querySelector(".fourth").addEventListener('click', toggleMsg)
+/* EVENT LISTENERS */
 
-// BREAKDOWN OF unreadNotifications
-// notificationsContainer.children returns an array-like obj which cannot be iterated by forEach
-// so it is converted to an array with Array.from
+// Marks clicked notification as read
+let notifications = Array.from(notificationsContainer.children);
+
+notifications.forEach(notification => notification.addEventListener('click', () => {
+    notification.setAttribute("class", "notification read");
+    notification.removeEventListener('click', () => {
+        notification.setAttribute("class", "notification read");
+    })
+}));
+
+// Finds the No of unread notifications
+window.onload = unreadNotificationsCounter();
+notifications.forEach(notification => notification.addEventListener('click', unreadNotificationsCounter));
+
+// Shows every notification as read
+allRead.addEventListener('click', markAllAsRead);
+
+// Toggles the 4th's notification response message apperance
+document.querySelector(".fourth").addEventListener('click', toggleMsg)
